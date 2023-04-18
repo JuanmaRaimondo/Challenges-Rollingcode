13a82b3d6e65a48f0269794d4cdf927100a2b74f
´´juanmaRaimondo´´ <juanmarraimondo@gmail.com>
2023-04-18 18:57:38 -0300

    subiendo

---
+++ Challenge13/app.js
@@ -0,0 +1,16 @@
+// crea tres variables y asignarle a dos valores numericos. mostrar en consola suma, resta multiplicacion y si es par o impar.
+
+let numero3 = 20;
+let numero4 = 10;
+let variable5 = "variable de tipo string";
+console.log(numero3+numero4 );
+console.log(numero3 - numero4);
+console.log(numero3 * numero4);
+console.log(numero3 / numero4);
+
+// a traves de document.write concatenar un saludo almacenado en dos variables
+
+let saludo1 = "Buenas tardes";
+let saludo2 = "comision 48i";
+document.write(saludo1+saludo2);
+
